236c27ec273ef20202a1c80dbf991d0148598e8f
Sarthak <sarthak.tiwari@mobilecoderz.com>
2022-05-18 13:44:51 +0530

    revert change

--- src/Screens/Demo/DemoNew.tsx
+++ src/Screens/Demo/DemoNew.tsx
@@ -62,7 +62,7 @@ function DemoNew() {
                 Activity
               </Typography>
               <Typography color="cornflowerblue" fontSize={25}>
-                Learning 
+                Learning Plan
               </Typography>
               <Typography color="cornflowerblue" fontSize={25}>
                 Progress
